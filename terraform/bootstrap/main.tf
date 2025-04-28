d1d89e2dd6e1f0cd637f846afdb68b641c2f82f0
avichayef <avichayef@newserver4.myguest.virtualbox.org>
2025-04-28 07:56:14 +0300

    S1-4 Completed + Bonus

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -50,41 +50,4 @@ resource "aws_dynamodb_table" "terraform_lock" {
   }
 }
 
-# Table to track bootstrap status
-resource "aws_dynamodb_table" "bootstrap_tracker" {
-  name           = "terraform-bootstrap-tracker"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "Environment"
 
-  attribute {
-    name = "Environment"
-    type = "S"
-  }
-}
-
-# Record that bootstrap ran
-resource "aws_dynamodb_table_item" "bootstrap_status" {
-  table_name = aws_dynamodb_table.bootstrap_tracker.name
-  hash_key   = "Environment"
-
-  item = jsonencode({
-    Environment = { S = "dev" }
-    LastRun     = { S = timestamp() }
-    BucketName  = { S = aws_s3_bucket.terraform_state.bucket }
-  })
-}
-
-# Comment out for dev env for now
-# resource "aws_secretsmanager_secret" "project_secrets" {
-#   name                    = "lawyal-secrets"  
-#   description            = "Initial secrets for Lawyal DevOps Project"
-#   recovery_window_in_days = 0
-# }
-
-# resource "aws_secretsmanager_secret_version" "project_secrets" {
-#   secret_id = aws_secretsmanager_secret.project_secrets.id
-#   secret_string = jsonencode({
-#     aws_access_key_id     = var.aws_access_key_id
-#     aws_secret_access_key = var.aws_secret_access_key
-#   })
-# }
